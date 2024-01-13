9a9f80e49de1627557fded111cd3e41806d054f4
toma tanaka <ulti4929@gmail.com>
2024-01-14 00:52:32 +0900

    update

--- src/ibldataset/main.py
+++ src/ibldataset/main.py
@@ -1,11 +1,12 @@
 from .codemodel import make_processing
 from .psuedodata import data_to_string
-from datasets import Dataset, DatasetDict
+import json
+from datasets import load_dataset, DatasetDict
 
 
-def make_dataset():
+def make_dataset(num_data):
     dataset = []
-    for j in range(3000):
+    for j in range(num_data):
         processing = make_processing()
         str_data = data_to_string(processing)
         data = {}
@@ -15,17 +16,30 @@ def make_dataset():
         data['category'] = 'regression'
         dataset.append(data)
 
-    dataset = {key: [dic[key] for dic in dataset] for key in dataset[0]}
-    dataset = Dataset.from_dict(dataset)
+    #dataset = {key: [dic[key] for dic in dataset] for key in dataset[0]}
+    #dataset = Dataset.from_dict(dataset)
     return dataset
 
 
-def make_dataset_dict():
-    train_dataset = make_dataset()
-    test_dataset = make_dataset()
+def preserve_dataset(dataset, path):
+    with open(path, 'w', encoding='utf-8') as file:
+        for record in dataset:
+            json_record = json.dumps(record, ensure_ascii=False)
+            file.write(json_record + '\n')
+    return
 
+
+def upload_dataset(train_path, test_path, dataset_name):
+    train_dataset = load_dataset("json", data_files = train_path)
+    test_dataset = load_dataset("json", data_files = test_path)
     dataset_dict = DatasetDict({
-        'train': train_dataset,
-        'test': test_dataset
-    })
-    return dataset_dict
+        'train': train_dataset['train'],
+        'test': test_dataset['train']
+        })
+    dataset_dict.push_to_hub(f"fuyu-quant/{dataset_name}")
+    return
+
+
+def download_dataset(data_name):
+    dataset = load_dataset(f"fuyu-quant/{data_name}", split='train')
+    return dataset
